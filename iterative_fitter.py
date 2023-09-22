cf7671b50508ec75d016b66ed761628248f312d7
Jacob Shpiece <js4664@drexel.edu>
2023-09-22 15:23:26 -0400

    Timing improvements

--- iterative_fitter.py
+++ iterative_fitter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
+import time
 
 sigf = 0.01
 sigs = 0.1
@@ -81,33 +82,50 @@ def chi2wrapper(guess,params):
     return chi2(params[0],params[1],params[2],params[3],params[4],params[5],guess[0],guess[1],guess[2],params[6],params[7])
     
 
-def list_winnower(xl,yl,eR):
-    #Given a set of lens candidates, remove any that aren't 
-    #considered viable. Follow the following rules
-    #1. If there are multiple lenses within 1 arcsecond of each other,
-    #   only keep the one with the highest eR
-    #2. If there are any lenses with negative einstein radii, remove them
-    #3. If there are any lenses with eR > 60, remove them
-    #4. If there are any lenses with eR < 1, remove them
-    #5. If there are lenses outside of the image, remove them
-
-    #print('Starting with ',len(eR),' candidate lenses')
+def list_winnower(xl,yl,eR,x,y):
+    #Given a set of lens candidates, remove any that aren't realistic
 
     #First, remove any lenses with negative einstein radii - as well as any too small to be realisticly detected
     #Also remove any lenses with eR > 60 (these are likely to be spurious)
     good = np.where((eR > 10**-3) & (eR < 60))[0]
-    #print('Removing ',len(eR)-len(good),' lenses with negative eR')
     xl = xl[good]
     yl = yl[good]
     eR = eR[good]
 
     #Next, remove any lenses outside of the image
     good = np.where((xl > -100) & (xl < 100) & (yl > -100) & (yl < 100))[0]
-    #print('Removing ',len(eR)-len(good),' lenses outside of the image')
     xl = xl[good]
     yl = yl[good]
     eR = eR[good] 
 
+    #Next, merge any lenses that are within 1 arcsecond of each other
+
+    for i in range(len(xl)):
+        for j in range(i+1,len(xl)):
+            if np.sqrt((xl[i]-xl[j])**2+(yl[i]-yl[j])**2) < 1.0:
+                #Merge these lenses
+                xl[i] = (xl[i]+xl[j])/2.0
+                yl[i] = (yl[i]+yl[j])/2.0
+                eR[i] = (eR[i]+eR[j])/2.0
+                xl = np.delete(xl,j)
+                yl = np.delete(yl,j)
+                eR = np.delete(eR,j)
+                break
+        else:
+            continue
+        break
+
+    #Finally, remove any lenses that are within 1 arcsecond of the source
+    #print(len(xl))
+    '''
+    for i in range(len(xl)):
+        for j in range(len(x)):
+            if np.sqrt((xl[i]-x[j])**2+(yl[i]-y[j])**2) < 1.0:
+                xl = np.delete(xl,i)
+                yl = np.delete(yl,i)
+                eR = np.delete(eR,i)
+                break
+    '''
     return xl,yl,eR
 
 
@@ -138,18 +156,20 @@ def iterative_minimizer(xlens,ylens,telens,chi2val):
 
 if __name__ == '__main__':
     # Set up the true lens configuration
-    nlens=3 # Number of lenses
+    start = time.time()
+    nlens=1 # Number of lenses
     xmax=10. # The range to consider for the lenses.
     xlarr,ylarr,tearr=createLenses(nlens=nlens,randompos=False,xmax=xmax)
 
     # Source parameters
-    ns=10 # Number of sources
+    ns=5 # Number of sources
     x,y,e1data,e2data,f1data,f2data=createSources(xlarr,ylarr,tearr,ns=ns,sigf=sigf,sigs=sigs,randompos=True,xmax=xmax)
 
     #perform local minimization
     xlens = np.zeros(ns)
     ylens = np.zeros(ns)
     telens = np.zeros(ns)
+    minimize_start = time.time()
     for i in range(ns):
         #Perform a local minimization\
         params = [[x[i]],[y[i]],[e1data[i]],[e2data[i]],[f1data[i]],[f2data[i]],sigf,sigs]
@@ -158,20 +178,74 @@ if __name__ == '__main__':
         xlens[i] = res.x[0]
         ylens[i] = res.x[1]
         telens[i] = res.x[2]
-    chi2val = chi2(x,y,e1data,e2data,f1data,f2data,xlens,ylens,telens,sigf,sigs)
+    #chi2val = chi2(x,y,e1data,e2data,f1data,f2data,xlens,ylens,telens,sigf,sigs)
+    minimize_end = time.time()
+
+    #Plot the results of each step
+    '''
+    fig, ax = plt.subplots(1,3,figsize=(16,4))
+
+    ax[0].scatter(xlarr,ylarr,s=tearr*50,color='red',marker='x',label='True Lenses',alpha=1.0)
+    ax[0].scatter(xlens,ylens,color='blue',s=np.abs(telens)*100,label='Candidate Lenses: nl = {}'.format(len(xlens)),alpha=0.70)
+    ax[0].scatter(x,y,color='green',s=10,label='Sources',alpha=0.70)
+    ax[0].set_xlim(-xmax,xmax)
+    ax[0].set_ylim(-xmax,xmax)
+    ax[0].set_aspect('equal')
+    ax[0].legend()
+    ax[0].set_title('Candidate Lenses from Local Minimization \n chi^2 = '+str(np.round(chi2val,3)))
+    '''
 
     #Perform the winnowing
-    xlens, ylens, telens = list_winnower(xlens,ylens,telens)
-    #Perform iterative minimization
-    xlens, ylens, telens, chi2val = iterative_minimizer(xlens,ylens,telens,chi2val)
+    winnowing_start = time.time()
+    xlens, ylens, telens = list_winnower(xlens,ylens,telens,x,y)
+    chi2val = chi2(x,y,e1data,e2data,f1data,f2data,xlens,ylens,telens,sigf,sigs)
+    winnowing_end = time.time()
 
+    #Plot the results of the winnowing
+    '''
+    ax[1].scatter(xlarr,ylarr,s=tearr*50,color='red',marker='x',label='True Lenses',alpha=1.0)
+    ax[1].scatter(xlens,ylens,color='blue',s=telens*50,label='Candidate Lenses: nl = {}'.format(len(xlens)),alpha=0.70)
+    ax[1].set_xlim(-xmax,xmax)
+    ax[1].set_ylim(-xmax,xmax)
+    ax[1].set_aspect('equal')
+    ax[1].legend()
+    ax[1].set_title('Candidate Lenses after Winnowing \n chi^2 = '+str(np.round(chi2val,3)))
+    '''
+
+    #Perform iterative minimization
+    iteration_start = time.time()
+    xlens, ylens, telens, chi2val = iterative_minimizer(xlens,ylens,telens,chi2val)
+    iteration_end = time.time()
+
+    #Plot the results of the iterative minimization
+    '''
+    ax[2].scatter(xlarr,ylarr,s=tearr*50,color='red',marker='x',label='True Lenses',alpha=1.0)
+    ax[2].scatter(xlens,ylens,color='blue',s=telens*50,label='Candidate Lenses: nl = {}'.format(len(xlens)),alpha=0.70)
+    ax[2].set_xlim(-xmax,xmax)
+    ax[2].set_ylim(-xmax,xmax)
+    ax[2].set_aspect('equal')
+    ax[2].legend()
+    ax[2].set_title('Candidate Lenses after Iterative Elimination \n chi^2 = '+str(np.round(chi2val,3)))
+    '''
+    end = time.time()
+    print('Time elapsed: ',end-start)
+    print('Time elapsed in minimization: ',minimize_end-minimize_start)
+    print('Time elapsed in winnowing: ',winnowing_end-winnowing_start)
+    print('Time elapsed in iteration: ',iteration_end-iteration_start)
+    #plt.savefig('two_lenses_fit.png')
+    #plt.show()
+
+
+
+    '''
     #Plot the results
     plt.figure()
     plt.title('Lensing field reconstruction \n chi^2 = '+str(np.round(chi2val,3)))
     plt.scatter(xlarr,ylarr,s=tearr*50,color='red',marker='x',label='True Lenses',alpha=1.0)
-    plt.scatter(xlens,ylens,color='blue',s=telens*50,label='Recovered Lenses: eR = '+str(np.round(telens,3)),alpha=1.0)
+    plt.scatter(xlens,ylens,color='blue',s=telens*50,label='Recovered Lenses: eR = '+str(np.round(telens,3)),alpha=0.70)
     plt.xlim(-xmax,xmax)
     plt.ylim(-xmax,xmax)
     plt.gca().set_aspect('equal')
     plt.legend()
     plt.show()
+    '''
---
+++ thesis work/Algorithm.md
@@ -0,0 +1,1 @@
+1. Perform a local minimization for each source, 
