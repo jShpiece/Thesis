998538931bceeca4698f67b490acc85a9726cca3
Jacob Shpiece <js4664@drexel.edu>
2023-10-03 15:38:35 -0400

    Implemented new tests to iterative elimination of sources

--- iterative_fitter.py
+++ iterative_fitter.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.optimize as opt
 import pipeline
 from utils import lens
+import time
 
 sigf = 0.01
 sigs = 0.1
@@ -46,24 +46,26 @@ def createSources(xlarr,ylarr,tearr,ns=1,randompos=True,sigf=0.1,sigs=0.1,xmax=5
     return x,y,e1data,e2data,f1data,f2data
 
 
-
-if __name__ == '__main__':
+def simple_implementation():
+    #Runs a very simple test of the algorithm, no inputs required
     '''Note to self - chi2 should be calculated per degree of freedom'''
+    start = time.time()
     # Set up the true lens configuration
-    nlens=1 # Number of lenses
-    xmax=4. # The range to consider for the lenses.
+    nlens = 2 # Number of lenses
+    xmax = 2 # The range to consider for the lenses.
     xlarr,ylarr,tearr=createLenses(nlens=nlens,randompos=True,xmax=xmax)
 
     # Set up the true source configuration
-    ns = 4
-    x,y,e1data,e2data,f1data,f2data=createSources(xlarr,ylarr,tearr,ns=ns,sigf=sigf,sigs=sigs,randompos=True,xmax=xmax)
+    ns = 2
+    x,y,e1data,e2data,f1data,f2data = createSources(xlarr,ylarr,tearr,ns=ns,sigf=sigf,sigs=sigs,randompos=True,xmax=xmax)
 
     # Run the minimization
-    xlens,ylens,eRlens,chi2 = pipeline.perform_minimization(x,y,e1data,e2data,f1data,f2data,sigs=sigs,sigf=sigf,xmax=xmax,flags=True)
-
+    xlens,ylens,eRlens,chi2val = pipeline.perform_minimization(x,y,e1data,e2data,f1data,f2data,sigs=sigs,sigf=sigf,xmax=xmax,flags=False)
+    stop = time.time()
+    print('Time elapsed: ', stop-start)
     # Plot the results
     plt.figure()
-    plt.scatter(xlens,ylens,color='red',label='Recovered Lenses')
+    plt.scatter(xlens,ylens,color='red',label=r'Recovered Lenses: $\theta_E$ = {}'.format(eRlens))
     plt.scatter(x,y,color='blue',label='Sources')
     plt.scatter(xlarr,ylarr,marker='x',color='green',label='True Lenses')
     plt.legend()
@@ -72,4 +74,41 @@ if __name__ == '__main__':
     plt.xlim(-xmax,xmax)
     plt.ylim(-xmax,xmax)
     plt.gca().set_aspect('equal')
+    plt.title('Lensing Reconstruction \n $\chi^2$ = %.2f' % chi2val)
     plt.show()
+
+
+
+def bulk_test(ntests): 
+    Nsources = [1,2,3,4,5,6,7,8,9,10]
+
+    print('| Nsources | % of solutions empty | % of solutions worse than true solution |')
+    for N in Nsources:
+        nempty = 0
+        n_badfit = 0
+        for i in range(ntests):
+            # Set up the true lens configuration
+            nlens = 1
+            xmax = 5 #Range of our lensing field - distance from the origin
+
+            xlarr,ylarr,tearr=createLenses(nlens=nlens,randompos=True,xmax=xmax)
+
+            # Set up the true source configuration
+            ns = 2*N
+            x,y,e1data,e2data,f1data,f2data = createSources(xlarr,ylarr,tearr,ns=ns,sigf=sigf,sigs=sigs,randompos=True,xmax=xmax)
+            true_chi2 = pipeline.chi2(x,y,e1data,e2data,f1data,f2data,xlarr,ylarr,tearr,sigf,sigs)
+
+            # Run the minimization
+            xlens,ylens,eRlens,chi2val = pipeline.perform_minimization(x,y,e1data,e2data,f1data,f2data,sigs=sigs,sigf=sigf,xmax=xmax,flags=False)
+            if len(xlens) == 0:
+                nempty += 1
+
+            if chi2val > true_chi2:
+                n_badfit += 1
+        
+        #Print results 
+        print('| {} | {} | {} |'.format(N, nempty/ntests, n_badfit/ntests))
+
+
+if __name__ == '__main__':
+    bulk_test(1000)
